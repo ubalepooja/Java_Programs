a55278cf3eb97017d1de5e2679d97d525f7a2de9
ubalepooja <ubalepooja77@gmail.com>
2024-03-19 12:24:57 +0530

    array file  added

---
+++ array.js
@@ -0,0 +1,13 @@
+let arr=new Array(2,3,4,5,6,7,8,9,10);
+console.log(arr);
+console.log(arr[2]); 
+arr.push(12); //push the particular no at the end
+console.log(arr);
+arr.pop();  //removes the last no from array
+console.log(arr);
+arr.unshift(1); //puts the no mentioned in round bracket at 0 array index.
+console.log(arr);
+arr.shift(); //removes the no present at zero index.
+console.log(arr);
+arr.splice(0,0,0);
+console.log(arr);
